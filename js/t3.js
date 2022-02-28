5793eb23ee46a96afe5585a635ec89f6eb4a8be9
Henry He <38474753+heshuhao218@users.noreply.github.com>
2022-02-28 14:01:17 -0500

    Add files via upload

---
+++ js/t3.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+alert("ATTENTION!");
+$("#h1").draggable();
+$("#part1").hover(function(){
+$("#catv2").show();
+});
+$("#part3").click(function(){
+$("#part2").hide();
+});
+$("#catv1").hover(function(){
+$("#s1").append("catcatcatcatcatcatcat");
+});
+$("#icon1").click(function(){
+// $("#img1").css('opacity',"Math.floor(Math.random()*5+5) + Math.ceil(Math.random()*10)/10");
+$("#part2").css("color","#fcdcfa");
+});
+
+})
